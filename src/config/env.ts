8ed29bf238c12107279d6aecfbb076004ae31dd1
ankush <ankush@azafashions.com>
2026-08-10 17:55:00 +0530

    refactor: support configurable base URL

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,2 +1,4 @@
+
+// Use environment BASE_URL or default to RESTful Booker
 export const BASE_URL =
-    "https://restful-booker.herokuapp.com";
+    process.env.BASE_URL ?? "https://restful-booker.herokuapp.com";
