46abafe9dbed284ed83aa8ffc64023eab124c46d
ankush <ankush@azafashions.com>
2026-08-10 17:27:49 +0530

    refactor: clean up API fixtures

--- src/fixtures/api.fixture.ts
+++ src/fixtures/api.fixture.ts
@@ -1,7 +1,7 @@
 import { test as base } from "@playwright/test";
 import { ApiClient } from "@api/clients/api.client";
 import { BookingClient } from "@api/clients/booking.client";
-import { getAuthToken } from "src/auth/auth";
+import { getAuthToken } from "@auth/auth";
 
 // DEFINE: Define the objects that our custom test will provide
 type ApiFixtures = {
@@ -10,33 +10,33 @@ type ApiFixtures = {
     authToken: string;
 }
 
-// EXTEND: Extend Playwright's test with our custom fixtures
+// Extend Playwright's test with our custom fixtures
 export const test = base.extend<ApiFixtures>({
 
-    // CREATE: Create ApiClient using Playwright's built-in request fixture
+    // Create ApiClient using Playwright's built-in request fixture
     apiClient: async ({ request }, use) => {
 
         const apiClient = new ApiClient(request);
 
-        // USE: Make ApiClient available to the test
+        // Make ApiClient available to the test
         await use(apiClient);
     },
 
-    // CREATE: Create BookingService using our ApiClient fixture
+    // Create BookingClient using our ApiClient fixture
     bookingClient: async ({ apiClient }, use) => {
 
         const bookingClient = new BookingClient(apiClient);
 
-        // USE: Make BookingService available to the test
+        // Make BookingClient available to the test
         await use(bookingClient);
     },
 
-    // CREATE: Generate authentication token
+    // Generate authentication token
     authToken: async ({ request }, use) => {
 
         const token = await getAuthToken(request);
 
-        // USE: Make authentication token available to the test
+        // Make authentication token available to the test
         await use(token);
     }
 });
